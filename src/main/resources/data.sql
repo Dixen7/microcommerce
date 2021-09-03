acd38137cc494e873c928825ba1ba3bac1b79237
Dixen7 <dixenlive@gmail.com>
2021-09-03 14:38:16 +0200

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+-- CREATE TABLE product;
+INSERT INTO product VALUES(1, 'Ordinateur portable' , 350, 120);
+INSERT INTO product VALUES(2, 'Aspirateur Robot' , 500, 200);
+INSERT INTO product VALUES(3, 'Meuble' , 650, 400);
